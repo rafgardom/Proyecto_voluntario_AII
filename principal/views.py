dee2cbdc6244789e44c7ddb455e8b3d36c326f5f
rafgardom <rafae@DESKTOP-H74978B>
2018-01-09 08:58:38 +0100

    Prueba de subida a git

--- principal/views.py
+++ principal/views.py
@@ -113,7 +113,7 @@ def set_profile(request):
         teams = usuario.favourite_teams
 
         #TODO voy por el la gestion del perfil. Primero muestro el perfil ya definido en plan los equipos favoritos y sus amigos. Lo proximo es en este mismo metodo poner el añadir amigos/equipos favoritos
-
+        #TODO prueba de subida a git
 
         return render_to_response('main.html', {'db_status': "Equipos y deportes generados", 'user': usuario})
 
