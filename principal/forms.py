dda819f057158a86b53dd3676c0f5f6b58f3ae77
rafgardom <rafae@DESKTOP-H74978B>
2018-01-10 02:36:32 +0100

    Corrección de errores Gestión de perfil completada: -Añadir equipo -Añadir amigo (Se controla que ni el equipo ni el amigo esté añadido ya)

--- principal/forms.py
+++ principal/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from models import Equipo, Deporte, Usuario
 
 class create_user(forms.Form):
     user_name = forms.CharField(max_length=20, required = True, label='Nombre de usuario')
@@ -7,3 +8,6 @@ class create_user(forms.Form):
     surname = forms.CharField(max_length=100, required=True, label='Apellidos')
     email = forms.EmailField(required=True)
     address = forms.CharField(max_length=300, required=True, label='Direccion')
+
+class select_friend_form(forms.Form):
+    usuarios = forms.ModelChoiceField(queryset=Usuario.objects.all())
